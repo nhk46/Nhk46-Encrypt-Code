491477beb885ad2bc56562f4218fbfabe452b32d
nhk46 <42830416+nhk46@users.noreply.github.com>
2019-08-22 02:04:35 +0700

    Nhk46-Encrypt-Code

---
+++ Nhk46-Encrypt-Code-3-Sh.sh
@@ -0,0 +1,11 @@
+#About : Yudi Leo Shuikoden Nhk-46
+#Lock   : Sh
+#2010-10-10
+#TERIMAKASIH
+
+
+
+
+z="
+";iz='\n"';yz='oden';Vz='ikod';Mz='° ͜ʖ';fz='ಠ(͡°';JBz='1;1m';NBz='k-46';YBz='scat';gBz='e.sh';Oz='͡° ͜';bBz='file';Dz='3;1m';FBz=': Lo';BBz='-46"';vz='di L';IBz='e"';OBz='"';cBz='.sh ';xz='huik';WBz='ash-';HBz='ncod';fBz='-fil';uz=': Yu';pz='🔍"';jz='6;1m';Nz=' ͡°(';Hz='★͡° ';qz='5;1m';hBz='2;1m';Ez='＝★＝★';wz='eo S';QBz='File';iBz='";';Wz='en N';Tz=' Leo';VBz=' $ b';SBz='🔎 C';KBz='Abou';LBz='t   ';Iz='͜ʖ ͡';lz='Prog';Bz=' "\0';aBz='ama-';Sz='Yudi';cz=')ಠ_ಠ';mz='ram ';Pz='ʖ ͡°';ez='°)ಠ_';Fz='＝＝★＝';bz='ಠ_ಠ(';nz='Encr';gz='͡°)＝';dz='(͡° ';hz='★';MBz=': Nh';Xz='hk-4';CBz='4;1m';Lz='͡°)͡';eBz='baru';RBz=' =\n';PBz='Isi ';Cz='33[3';UBz='h 👉';ABz=' Nhk';Az='echo';ZBz='e  n';Jz='°(͡°';Kz=' ͜ʖ ';kz=' 🔎 ';TBz='onto';Zz='★＝(͡';Rz=' ͡°★';az=' ͡°)';XBz='obfu';rz=' 👉 ';Yz='6';oz='ypt ';Gz='★＝★＝';dBz='-o n';tz='    ';EBz='-Sh ';Uz=' Shu';Qz=')͡° ';DBz='Bash';GBz='ck E';sz='Nama';
+eval "$Az$Bz$Cz$Dz$z$Ez$Ez$Ez$Ez$Fz$Gz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Iz$Jz$Kz$Lz$Mz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Yz$z$Zz$Mz$az$bz$Oz$Pz$cz$dz$Iz$ez$fz$Kz$gz$hz$z$Ez$Ez$Ez$Ez$Fz$Gz$Gz$iz$z$Az$Bz$Cz$jz$kz$lz$mz$nz$oz$pz$z$Az$Bz$Cz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$z$Az$Bz$Cz$CBz$rz$DBz$EBz$FBz$GBz$HBz$IBz$z$Az$Bz$Cz$JBz$rz$KBz$LBz$MBz$NBz$OBz$z$Az$Bz$Cz$Dz$rz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$aBz$eBz$fBz$gBz$OBz$z$Az$Bz$Cz$hBz$iBz"
